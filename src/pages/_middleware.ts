c60bd4c089f6886d38b9c322825e5e68a341af81
carlos <chlouzada@gmail.com>
2022-08-08 13:56:31 -0300

    fix middleware on api route

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -4,14 +4,16 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  if (req.url.indexOf("/api/new") != -1) return;
+
   const split = req.nextUrl.pathname.split("/").pop();
 
   if (split === "") return;
 
-  const res = await fetch(req.nextUrl.origin + "/api/redirect/" + split)
+  const res = await fetch(req.nextUrl.origin + "/api/redirect/" + split);
   const data = await res.json();
 
-  if(!data.url) return NextResponse.redirect(req.nextUrl.origin)
+  if (!data.url) return NextResponse.redirect(req.nextUrl.origin);
 
-  return NextResponse.redirect(data.url)
+  return NextResponse.redirect(data.url);
 }
